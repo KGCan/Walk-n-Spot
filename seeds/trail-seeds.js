91586976d5d4580f8aa3ced9601a70cc443582ab
PacDraft <Kirsten@PacificDraftWorks.com>
2022-01-12 20:54:53 -0800

    removed modals from main.handlebars

--- seeds/trail-seeds.js
+++ seeds/trail-seeds.js
@@ -3,27 +3,27 @@ const { Trail } = require('../models');
 const trailData = [
     {
         trail_name: 'American River Parkway Trailhead',
-        coordinates: '38.6010° N, 121.5077° W',
+        coordinates: '38.6010, -121.5077',
         animal_id: 1
     },
     {
         trail_name: 'Sacramento River Parkway',
-        coordinates: '38.5838° N, 121.5060° W',
+        coordinates: '38.5838, -121.5060',
         animal_id: 1
     },
     {
         trail_name: 'Riverfront Promenade',
-        coordinates: '38.5770° N, 121.5086° W',
+        coordinates: '38.5770, -121.5086',
         animal_id: 1
     },
     {
         trail_name: 'Creekside Park & Nature Area',
-        coordinates: '38.6140° N, 121.3938° W',
+        coordinates: '38.6140, -121.3938',
         animal_id: 1
     },
     {
         trail_name: 'Bannon Creek Park & Parkway',
-        coordinates: '38.6146° N, 121.5046° W',
+        coordinates: '38.6146, -121.5046',
         animal_id: 1
     }
 ]
