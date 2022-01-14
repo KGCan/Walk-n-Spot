a50b32b5fdc6ff0cbb8e9200178c4d6352bb5194
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2022-01-14 11:09:17 -0800

    userTrail model, seeds, tables

--- models/index.js
+++ models/index.js
@@ -1,19 +1,59 @@
-
 const User = require("./User");
 const Trail = require("./Trail")
 // const City = require("./City")
 // const Sighting = require("./Sighting")
 const Animal = require("./Animal");
 const TrailAnimal = require("./TrailAnimal");
+const UserTrail = require("./UserTrail")
 
-// City.hasMany(Trail, {
-//     foreignKey: 'city_id'
+
+
+//   -------   Trail & User Associations Start -------
+User.belongsToMany(Trail, {
+  through: UserTrail,
+  // foreignKey: 'user_id'
+});
+
+Trail.belongsToMany(User, {
+  through: UserTrail,
+  // foreignKey: 'trail_id'
+});
+
+
+
+User.hasMany(UserTrail, {
+  // foreignKey: 'user_id'
+})
+UserTrail.belongsTo(User, {
+  // foreignKey: 'user_id'
+});
+Trail.hasMany(UserTrail, {
+  // foreignKey: 'trail_id'
+})
+UserTrail.belongsTo(Trail, {
+  // foreignKey: 'trail_id'
+});
+
+
+// User.hasMany(Trail, {
+//   foreignKey: 'user_id'
 // });
 
-// Trail.belongsTo(City, {
-//     foreignKey: 'city_id'
+// Trail.hasMany(User, {
+//   foreignKey: 'trail_id'
 // });
 
+// // Trail.hasMany(User, {
+// //   foreignKey: 'user_id'
+// // });
+
+// Trail.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
+//   -------   Trail & User Associations END -------
+
+
+//  ------- Trail &  Animal Associations START --------
 Animal.belongsToMany(Trail, {
   through: TrailAnimal,
   foreignKey: 'animal_id'
@@ -39,22 +79,35 @@ Animal.hasMany(TrailAnimal, {
 TrailAnimal.belongsTo(Animal, {
   foreignKey: 'animal_id'
 })
+//  ------- Trail &  Animal Associations END --------
 
-User.hasMany(Trail, {
-    foreignKey: 'user_id'
-});
 
-Trail.hasMany(User, {
-  foreignKey: 'trail_id'
-});
 
-// Trail.hasMany(User, {
-//   foreignKey: 'user_id'
+
+module.exports = { User, Trail, Animal, TrailAnimal }
+
+
+
+
+
+
+
+
+
+
+
+
+//  ---------------- DRAFT CODE  ----------------------
+
+
+// City.hasMany(Trail, {
+//     foreignKey: 'city_id'
 // });
 
-Trail.belongsTo(User, {
-    foreignKey: 'user_id'
-});
+// Trail.belongsTo(City, {
+//     foreignKey: 'city_id'
+// });
+
 
 // User.hasMany(Sighting, {
 
@@ -68,5 +121,3 @@ Trail.belongsTo(User, {
 
 // });
 
-
-module.exports = { User, Trail, Animal, TrailAnimal } 
--- seeds/index.js
+++ seeds/index.js
@@ -2,11 +2,39 @@ const seedAnimal = require('./animal-seeds');
 const seedTrail = require('./trail-seeds');
 // const seedCity = require('./city-seeds');
 const seedUser = require('./user-seeds');
+const seedUserTrail = require('./usertrail-seeds');
+
 
 const sequelize = require('../config/connection');
 
+sequelize.query('SET FOREIGN_KEY_CHECKS = 0')
+  .then(function () {
+    return sequelize.sync({ force: true });
+  })
+  .then(function () {
+    return sequelize.query('SET FOREIGN_KEY_CHECKS = 1')
+  })
+  .then(function () {
+    console.log('Database synched.');
+  }, function (err) {
+    console.log(err);
+  });
+
+//async.series([
+// function (callback) {
+//   sequelize.query("SET FOREIGN_KEY_CHECKS = 0").complete(callback);
+// },
+// function (callback) {
+//   sequelize.sync({ force: true }).complete(callback);
+// },
+// function (callback) {
+//   sequelize.query("SET FOREIGN_KEY_CHECKS = 1").complete(callback);
+// }]
+// , callback);
+
+
 const seedAll = async () => {
-  await sequelize.sync({ force: false });
+  await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
 
   await seedAnimal();
@@ -20,6 +48,9 @@ const seedAll = async () => {
 
   await seedUser();
   console.log('\n----- User SEEDED -----\n');
+
+  await seedUserTrail();
+  console.log('\n----- UserTrail SEEDED -----\n');
 
   process.exit(0);
 };
