05d324a0029d5aa72238ede129338918adce3499
Michael Li <thisislimichael@gmail.com>
2022-01-15 12:46:08 -0800

    fixed the login/signup/logout routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,10 @@ const sequelize = require('../config/connection');
 //const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    console.log(req.session);
+    res.render('homepage', {
+        loggedIn: req.session.loggedIn
+    });
 });
 
 router.get('/login', (req, res) => {
