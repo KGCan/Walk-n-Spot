f1bac25cbabfff2d9998bb0982b85c7414107306
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2022-01-15 23:46:48 -0800

    tested user routes: post 2x get1 & delete successfl; PUT needs retest

--- server.js
+++ server.js
@@ -45,6 +45,6 @@ const routes = require('./controllers');
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
