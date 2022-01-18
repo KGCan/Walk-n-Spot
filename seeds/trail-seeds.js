6718ed30bb1d8600cd4b1ee28c951b413e4be6d7
Michael Li <thisislimichael@gmail.com>
2022-01-17 21:14:04 -0800

    fixed seeds and model

--- seeds/trail-seeds.js
+++ seeds/trail-seeds.js
@@ -7,7 +7,6 @@ const trailData = [
         lat: '38.6010',
         lon: '-121.5077',
         trail_img: './assets/images/creek-1-sm.jpg',
-        alltrails: 'https://www.alltrails.com/trail/us/california/american-river-bike-trail-jedediah-smith-memorial-trail',
         animal_id: 1,
         trail_info: 'https://regionalparks.saccounty.net/Parks/Pages/AmericanRiverParkway.aspx'
     },
@@ -16,7 +15,7 @@ const trailData = [
         city_name: 'SACRAMENTO',
         lat: '38.5838',
         lon: '-121.5060',
-        trail_img: './public/assets/images/creek-2.jpg',
+        trail_img: './assets/images/creek-2.jpg',
         animal_id: 1,
         trail_info: "https://www.cityofsacramento.org/Public-Works/Engineering-Services/Projects/Current-Projects/Sacramento-River-Parkway"
     },
@@ -25,7 +24,7 @@ const trailData = [
         city_name: 'SACRAMENTO',
         lat: '38.5770',
         lon: '-121.5086',
-        trail_img: './public/assets/images/creek-3.jpg',
+        trail_img: './assets/images/creek-3.jpg',
         animal_id: 2,
         trail_info: "https://www.cityofsacramento.org/Public-Works/Engineering-Services/Projects/Current-Projects/Sacramento-River-Parkway"
 
@@ -35,7 +34,7 @@ const trailData = [
         city_name: 'Sacramento',
         lat: '38.6140',
         lon: '-121.3938',
-        trail_img: './public/assets/images/creek-3.jpg',
+        trail_img: './assets/images/creek-3.jpg',
         animal_id: 7,
         trail_info: "https://www.fecrpd.com/creekside-nature-area"
 
@@ -45,7 +44,7 @@ const trailData = [
         city_name: 'Sacramento',
         lat: '38.6146',
         lon: '-121.5046',
-        trail_img: './public/assets/images/creek-3.jpg',
+        trail_img: './assets/images/creek-3.jpg',
         animal_id: 1,
         trail_info: "http://www.cityofsacramento.org/parksandrec/parks/park-directory/south-natomas/bannon-creek"
     }
