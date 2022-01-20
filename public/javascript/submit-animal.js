2fad6fd2cae59da58c63b00feed11c803bbf1fe5
Michael Li <thisislimichael@gmail.com>
2022-01-20 11:06:58 -0800

    added feature for users to add animals to trail if that trail did not have that animal sighting yet

--- public/javascript/submit-animal.js
+++ public/javascript/submit-animal.js
@@ -1,42 +1,18 @@
-// const router = require('express').Router();
-// // const withAuth = require('../utils/auth');
-// const sequelize = require('../../config/connection');
-// const { User, Trail, Animal, TrailAnimal, UserTrail } = require('../../models');
 
-// const { response } = require("express");
+/* 
+When user submits what animal he/she saw for a specific trail, a GET response will start first
+that gets the 'value' of the animal and trail. That value will be the trail/animal's id that is 
+currently being auto increment. Those values will be placed into the api route so user can get the 
+sighting count of that specific animal on the specified trail. 
 
+The sighting count is assigned to a variable and then the count is increased by 1. Then a PUT method 
+is started. With the same trail and animal value, the new sighting count is passed to this route
+so the sighting count can be updated in the database
 
-// async function submitAnimal(event) {
-
-// const { json } = require("express/lib/response");
-
-//     const response = await fetch('/api/animal/sighting', {
-//         method: 'GET',
-//         })
-//         .then(response => response.json())
-//         .then(function (json) {
-//             console.log('Success')
-//             const animal_submit = document.querySelector("select[name='animalSubmit']").id;    
-
-//             console.log(json)
-//             if(animal_submit != "All"){
-//                 for(var i =0; i <json.length; i++) {
-//                     for(var j=0; j< json[i].animals.length; j++) {
-//                         if (animal_submit === json[i].animals[j].animal_name) {
-//                             console.log(json[i].animals[j].trail_animal.sighting)
-//                         } else {
-//                             console.log('Weird')
-//                         }
-//                     }
-//                 }
-//             }//If look for all animals
-//             else {
-//                 console.log('All')
-//             }
-             
-//         });
-
-// }
+If there has been no sighting (null) of this specific animal on the specified trail then 
+a POST route will be started which will then pass the indicated trail/animal id and have a sighting 
+of 1
+*/
 
 function submitAnimal() {
     const trail_submit = document.querySelector("select[name='trailSubmit']").value;
@@ -53,145 +29,66 @@ function submitAnimal() {
        })
        .then(response => response.json())
        .then((json) => {
-           if (json) {
+           if (json.id) {
                 console.log(json)
-           } else {
-               console.log("Doesn't exist")
+                console.log('----------------------------------------Here is the sighting data------------------------------------');
+                console.log(json.sighting)
+                let oldSighting = json.sighting;
+                var newSighting = oldSighting + 1
+                var sighting = newSighting
+                console.log(sighting)
+                const trail_submit = document.querySelector("select[name='trailSubmit']").value;
+                const animal_submit = document.querySelector("select[name='animalSubmit']").value;
+                console.log(trail_submit)
+                console.log(animal_submit)
+                // var sighting = await submitAnimal()
+                console.log(sighting)
+                fetch(`/api/animal/${parseInt(trail_submit)}/${parseInt(animal_submit)}`, {
+                    method: 'PUT',
+                    body: JSON.stringify({
+                    sighting
+                }),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                }).then((res) => {
+                    document.location.replace('/dashboard');
+                    console.log('works')
+                })
+                .catch((err) => { console.warn(err)});
+           } else if (json.id == null) {
+                console.log("Doesn't exist")
+                const trail_id = document.querySelector("select[name='trailSubmit']").value;
+                const animal_id = document.querySelector("select[name='animalSubmit']").value;
+                console.log(trail_submit)
+                console.log(animal_submit)
+                var sighting = 1
+                console.log(sighting)
+                fetch(`/api/animal/${parseInt(trail_id)}/${parseInt(animal_id)}`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        trail_id,
+                        animal_id,
+                        sighting
+                }),
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                }).then((res) => {
+                    document.location.replace('/dashboard');
+                    console.log('works')
+                    return 
+                })
+                .catch((err) => { console.warn(err)});
            }
-            console.log('----------------------------------------Here is the sighting data------------------------------------');
-            console.log(json.sighting)
-            let oldSighting = json.sighting;
-            var newSighting = oldSighting + 1
-            var sighting = newSighting
-            console.log(sighting)
-            const trail_submit = document.querySelector("select[name='trailSubmit']").value;
-            const animal_submit = document.querySelector("select[name='animalSubmit']").value;
-            console.log(trail_submit)
-            console.log(animal_submit)
-            // var sighting = await submitAnimal()
-            console.log(sighting)
-            fetch(`/api/animal/${parseInt(trail_submit)}/${parseInt(animal_submit)}`, {
-                method: 'PUT',
-                body: JSON.stringify({
-                sighting
-            }),
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            },
-            }).then((res) => {
-                document.location.replace('/dashboard');
-                console.log('works')
-            })
-            .catch((err) => { console.warn(err)});
+
         })
         .catch((error) => { console.warn(error); })
 }
 
-
-
-
- 
-// function submitAnimal() {
-
-//     const trail_submit = parseInt(document.querySelector("select[name='trailSubmit']").value);
-//     const animal_submit = parseInt(document.querySelector("select[name='animalSubmit']").value);
-//     console.log(trail_submit)
-//     console.log(animal_submit)
-
-    
-
-//     fetch(`/api/animal/${trail_submit}/${animal_submit}/`, {
-//         method: 'GET',
-//         headers: {
-//             'Accept': 'application/json',
-//             'Content-Type': 'application/json',
-//         },
-//        })
-//        .then(response => response.json())
-//        .then((json) => {
-//         console.log('----------------------------------------Here is the sighting data------------------------------------');
-
-//             console.log(json.sighting)
-
-//             let oldSighting = json.sighting;
-            
-//             var newSighting = oldSighting + 1
-
-//             var sighting = newSighting
-//             console.log(sighting)
-
-//             const trail_submit = parseInt(document.querySelector("select[name='trailSubmit']").value);
-//             const animal_submit = parseInt(document.querySelector("select[name='animalSubmit']").value);
-
-//             console.log(trail_submit)
-//             console.log(animal_submit)
-
-//             // var sighting = await submitAnimal()
-//             console.log(sighting)
-            
-
-//             const response = fetch(`/api/animal/${trail_submit}/${animal_submit}/`, {
-//                 method: 'PUT',
-
-//                 body: JSON.stringify({
-//                 sighting
-//             })
-//             });
-//             if (response.ok) {
-//                 // document.location.replace('/dashboard');
-//                 console.log('works')
-//             } else {
-//                 alert('Select again');
-//             }
-//         //    return sighting
-//         })
-//         .catch((error) => { console.warn(error); })
-// }
-
-// async function updateSighting() {
-//     const trail_submit = document.querySelector("select[name='trailSubmit']").value;
-//     const animal_submit = document.querySelector("select[name='animalSubmit']").value;
-
-//     console.log(trail_submit)
-//     console.log(animal_submit)
-
-//     var sighting = await submitAnimal()
-//     console.log(sighting)
-
-//     const response = await fetch(`/api/animal/${trail_submit}/${animal_submit}/`, {
-//         method: 'PUT',
-
-//         body: JSON.stringify({
-//            sighting
-//        })
-//     });
-//     if (response.ok) {
-//         // document.location.replace('/dashboard');
-//         console.log('works')
-//     } else {
-//         alert('Select again');
-//     }
-// }
-
-    //  const response = await fetch(`/api/animal/${trail_submit}/${animal_submit}/`, {
-    //      method: 'PUT',
-
-    //      body: JSON.stringify({
-    //         sighting
-    //     })
-    //     })
-    //     .then(response => response.json())
-    //     .then(function(json) {
-    //         console.log(json)
-        
-    //     })
-
-
-
+// When button with class ".submitAnimal" in the dashboard.handlebars is clicked
+// will trigger submitAnimal function
 
 document.querySelector('.submitAnimal').addEventListener('click', submitAnimal);
-
-
-// Make a search bar to check input for the trail name and animal inputted, then make a 
-// function to increase that count and create a put request
--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 
 const sess = {
   secret: 'Animals Rock The Walk',
@@ -25,20 +22,16 @@ const sess = {
 // Session middleware, creates session, sets cookie, and session obj in req obj, so have session info stored if same user logs in again
 app.use(session(sess));
 
-
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
 
 //middleware to prepare req.body and serve static files
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 // turn on routes
 const routes = require('./controllers');
